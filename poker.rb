d4a06938416e36cb0039fe9b129e58227a4d96cb
Taiji Namba <namba9499@gmail.com>
2020-09-17 00:27:56 +0900

    役判定メソッド, Dealer手札交換メソッドリファクタリング

--- poker.rb
+++ poker.rb
@@ -84,24 +84,34 @@ class Hand
     count_number_of_suits_types
     count_number_of_scores_types
 
+    a_pair = @number_of_each_score.count(2) == 1
+    two_pair = @number_of_each_score.count(2) == 2
+    three_of_a_kind = @number_of_each_score.include?(3)
+    straight = @differences.count(1) == 4
+    flush = @number_of_suits_types == 1
+    a_full_house = a_pair && three_of_a_kind
+    four_of_a_kind = @number_of_each_score.include?(4)
+    straight_flush = straight && flush
+    royal_straight_flush = straight && flush && @sort_hands[0].score == 10
+
     #役判定
-    if @differences.count(1) == 4 && @number_of_suits_types == 1 && @sort_hands[0].score == 10
+    if royal_straight_flush
       @rank = "ローヤルストレートフラッシュ"
-    elsif @differences.count(1) == 4 && @number_of_suits_types == 1
+    elsif straight_flush
       @rank = "ストレートフラッシュ"
-    elsif @number_of_each_score.include?(4) 
+    elsif four_of_a_kind
       @rank = "フォーカード"
-    elsif @number_of_each_score.include?(3) && @number_of_each_score.include?(2)
+    elsif a_full_house
       @rank = "フルハウス"
-    elsif @number_of_suits_types == 1
+    elsif a_full_house
       @rank = "フラッシュ"
-    elsif @differences.count(1) == 4
+    elsif straight
       @rank = "ストレート"
-    elsif @number_of_each_score.include?(3)
+    elsif three_of_a_kind
       @rank = "スリーカード"
-    elsif @number_of_each_score.count(2) == 2
+    elsif two_pair
       @rank = "ツーペア"
-    elsif @number_of_each_score.count(2) == 1
+    elsif a_pair
       @rank = "ワンペア"
     else
       @rank = "ハイカード"
@@ -144,13 +154,13 @@ class Hand
     @suits << suit 
     }
     
-    #suitsの種類と数をhashで取得
+    #スートの種類とぞれぞれの枚数をhashで取得
     @hash_of_suits_types = @suits.group_by(&:itself).transform_values(&:size)
 
-    #suitsの中の同じ要素の数をhashで返し、hashの要素数を取得
+    #スートは何種類あるか
     @number_of_suits_types = @hash_of_suits_types.size
 
-    #同じsuitが何枚ずつあるか
+    #1つのスートがそれぞれ何枚ずつあるか配列で取得
     @number_of_each_suit = @hash_of_suits_types.values
   end
 
@@ -167,11 +177,23 @@ class Hand
     #scoreの種類と数をhashで取得
     @hash_of_scores_types = @scores.group_by(&:itself).transform_values(&:size)
 
-    #scoresの中の同じ要素の数をhashで返し、hashの要素数を取得
+    #scoreは何種類あるか
     @number_of_scores_types = @hash_of_scores_types.size
 
-    #同じscoreが何枚ずつあるか
+    #1つのscoreがそれぞれ何枚ずつあるか配列で取得
     @number_of_each_score = @hash_of_scores_types.values
+  end
+
+  def display_hands
+    @hands.each.with_index(1) do |hand, i|
+      puts "#{i}枚目 ： #{hand.show}"
+    end
+  end
+
+  def discard
+    @hands.delete_if{|hand|
+      @discards.include?(hand)
+    }
   end
 
 end
@@ -185,54 +207,12 @@ class Player < Hand
     -------------Player手札-------------
     EOS
 
-    @hands.each.with_index(1) do |hand, i|
-      puts "#{i}枚目 ： #{hand.show}"
-    end
+    display_hands
 
     puts "------------------------------------"
   end
 
-  #捨てる手札の選択
-  def select_discard
-    #交換メッセージ
-    puts <<~EOS
-
-    何枚目のカードを交換するか数字で入力して下さい
-    複数枚を選択する際は間に半角スペースをあけて下さい
-    例)1 2 3
-    EOS
-    
-    #捨てるカードの選択
-    @selected_ids = gets.split(' ').map(&:to_i)
-    
-    #選んだ手札を表示
-    puts 
-    @selected_ids.each{|id| 
-      selected_hand = @hands[id - 1]
-      puts selected_hand.show
-      @discards << selected_hand
-    }
-  end
-  
-  #選択した手札を捨てる
-  def player_discard
-    @hands.delete_if{|hand|
-      @discards.include?(hand)
-    }
-    puts <<~EOS
-      を交換しました
-    EOS
-  end
-    
-  #捨てた枚数分山札から引く
-  def draw_for_exchange(deck)
-    @selected_ids.size.times {|hand|
-      hand = deck.draw
-      @hands << hand
-    }   
-  end
-
-    #手札を交換するか決める
+      #手札を交換するか決める
   def decide_exchange(deck)
     puts <<~EOS
 
@@ -244,118 +224,267 @@ class Player < Hand
     if action == 1
       select_discard
       player_discard
-      draw_for_exchange(deck)
+      player_draw_for_exchange(deck)
     elsif action == 2
       return
     end
 
   end
+
+  #捨てる手札の選択
+  def select_discard
+    
+    select_message
+
+    #捨てるカードの選択
+    @selected_ids = gets.split(' ').map(&:to_i)
+    
+    puts 
+
+  end
+
+  #交換メッセージ
+  def select_message
+    puts <<~EOS
+
+    何枚目のカードを交換するか数字で入力して下さい
+    複数枚を選択する際は間に半角スペースをあけて下さい
+    例)1 2 3
+    EOS
+  end
+  
+  #選択した手札を捨てる
+  def player_discard
+    discard
+
+    #選択した手札を表示
+    @selected_ids.each{|id| 
+      selected_hand = @hands[id - 1]
+      puts selected_hand.show
+      @discards << selected_hand
+    }
+
+    puts <<~EOS
+      を交換しました
+    EOS
+  end
+    
+  #捨てた枚数分山札から引く
+  def player_draw_for_exchange(deck)
+    @selected_ids.size.times {|hand|
+      hand = deck.draw
+      @hands << hand
+    }   
+  end
+
 end
 
 class Dealer < Hand
+
   def dealer_exchange(deck)
     rearrange
     take_difference
     count_number_of_suits_types
     count_number_of_scores_types
-    
+    dealer_decide_card
+
+  end
+
+  def dealer_decide_card
+
+    #フラッシュドロー(4枚のスートが同じ)の定義
+    flush_draw = @number_of_each_suit.include?(4)
+    #ストレートドロー(1枚だけscoreが連続していない)の定義
+    straight_draw = @differences.count(1) == 3
 
     #ストレート系が完成済み
-    if @differences.count(1) == 4
+    if @rank == "ストレート"
       return #ストレート系を確定
 
     #フォーカードかフルハウスが完成済み
-    elsif @differences.count(0) == 3
+    elsif @rank == "フォーカード"
       return #フォーカード か フルハウスを確定
 
+    #フルハウスが完成済み
+    elsif @rank == "フルハウス"
+      return #フルハウスを確定
+
     #フラッシュが完成済み
-    elsif @number_of_suits_types == 1
+    elsif @rank == "フラッシュ"
       return #フラッシュを確定
 
-    #ストレートが完成済みで、スートが同じ札が4枚
-    elsif @number_of_each_suit == 4 && @differences.count(1) == 4
-      #1/3の確率でフラッシュ狙いで交換, 2/3の確率でストレートを確定
+    #スートが同じ札が4枚でストレートが完成済み
+    elsif flush_draw && @rank == "ストレート"
+      #1/3の確率でストレートを確定, 2/3の確率でフラッシュ(あるいはストレートフラッシュ狙い)で交換
       probability = rand(3)
-      probability == 0 ? action_for_flush : return
+      probability == 0 ? return : action_for_flush
     
-    #スートが同じ札が4枚で@differences.count(0) == 2
-    elsif @number_of_each_suit == 4 && @differences.count(0) == 2
-      if @suits[0] != @suits[1]
-        #フルハウス狙いで1枚目を交換
-        @sorts_hands[0].delete
-        
-        hand = deck.draw
-        @hands << hand
-
-      elsif @suits[3] != @suits[4]
-        #フルハウス狙いで4枚目を交換
-        @sorts_hands[3].delete
-        
-        hand = deck.draw
-        @hands << hand
-      else
-        return
-      end
-
     #スートが同じ札が4枚
-    elsif @number_of_each_suit == 4
-      #フラッシュ狙いで1枚交換
-      action_for_flush(deck)
+    elsif flush_draw
+      action_for_flush #フラッシュ狙いでスートが違う1枚交換
     
-    # @differences.count(1) == 3 かつ 2枚目-1枚目が1以上
-    elsif @differences.count(1) == 3 && @scores[1] - @scores[0] > 1
-      #ストレート狙いで1枚目を交換
-      @sorts_hands[0].delete
-
-      hand = deck.draw
-      @hands << hand
-
-    #@differences.count(1) == 3 かつ 5枚目-4枚目が1以上
-    elsif @differences.count(1) == 3 && @scores[4] - @scores[3] > 1
-      #ストレート狙いで5枚目を交換
-      @sorts_hands[4].delete
-
-      hand = deck.draw
-      @hands << hand
+    # @differences.count(1) == 3 かつ 1枚だけ2以上離れている
+    elsif straight_draw 
+      action_for_straight
       
-    #1,2枚目、4,5枚目の差が1で3枚目と4枚目の差が2のとき
-    elsif @scores[2] - @scores[1] == 1 && @scores[4] - @scores[3] == 1 && @scores[2] - @scores[3] == 2
-      #ストレート狙いで1枚目を交換
-      @sorts_hands[0].delete
-
-      hand = deck.draw
-      @hands << hand
-
     #スリーカード完成済み
-    elsif @number_of_each_score.include?(3)
-      if @scores[0] == @scores[1] && @scores[1] && @scores[2]
-        #フルハウス狙いで4枚目を交換
-        @sorts_hands[3].delete
+    elsif @rank == "スリーカード"
+      #スリーカードになっていない1枚交換
+      action_when_three_of_a_kind
 
-        hand = deck.draw
-        @hands << hand
-      end
-      
+    #ツーペア形成済み, ペアになっていない札を交換
+    elsif @rank == "ツーペア"
+      action_when_two_pair
+
+    #ワンペア形成済み, ペア以外を交換
+    elsif @rank == "ワンペア"
+      action_when_a_pair
 
     else 
-      puts "交換なし"
+      action_when_high_card
     end
+  end
 
-    #dealerの手札公開
-    @hands.each.with_index(1) do |hand, i|
-      puts "#{i}枚目 ： #{hand.show}"
-    end
+  #ストレート狙いのアクション
+  def action_for_straight
+    #1枚目のscoreが連続していない
+    if @scores[1] - @scores[0] >= 2
+      #1枚目を交換
+      @discards << @sorts_hands[0]
+      
+      dealer_draw(1)
+      
+    #5枚目のscoreが連続していない
+    elsif @scores[4] - @scores[3] >= 2
+      #5枚目を交換
+      @discards << @sorts_hands[4]
 
-    #フラッシュ狙いで1枚交換
-    def action_for_flush(deck)
-      #1枚だけ違うsuitのカードを交換
-      if suit = @suits.find{|suit| suit == @hash_of_suits_types.key(1)}
-        @sort_hands.delete(suit)
-        
-        hand = deck.draw
-        @hands << hand
-      end
+      dealer_draw(1)
+    
+    #2,3枚目もしくは3,4枚目が同じ
+    elsif @scores[1] == @scores[2] || @scores[2] == @scores[3]
+      #同じscoreの札を交換
+      @discards << @sorts_hands[2]
+
+      dealer_draw(1)
+
     end
+  end
+
+  #フラッシュ狙いの行動
+  def action_for_flush
+    #1枚だけ違うsuitのカードを交換
+    if suit = @suits.find{|suit| suit == @hash_of_suits_types.key(1)}
+      @sort_hands.delete(suit)
+      
+      hand = deck.draw
+      @hands << hand
+    end
+  end
+
+  #スリーペアのときの行動
+  def action_when_three_of_a_kind
+    #1,2,3枚目のscoreが同じ
+    if @scores[0] == @scores[1] && @scores[1] && @scores[2]
+      #フルハウス狙いで4枚目を交換
+      @discards << @sorts_hands[3]
+
+      dealer_draw(1)
+
+    #2,3,4枚目のscoreが同じ
+    elsif @scores[1] == @scores[2] && @scores[2] && @scores[3]
+      #フルハウス狙いで1枚目を交換
+      @discards << @sorts_hands[0]
+
+      dealer_draw(1)
+
+    #3,4,5枚目のscoreが同じ
+    elsif @scores[2] == @scores[3] && @scores[3] && @scores[4]
+      #フルハウス狙いで1枚目を交換
+      @discards << @sorts_hands[0]
+
+      dealer_draw(1)
+
+    end
+  end
+
+  #ツーペアのときの行動
+  def action_when_two_pair
+    if @scores[0] == @scores[1] && @scores[2] && @scores[3]
+      #フルハウス狙いで5枚目を交換
+      @discards << @sorts_hands[4]
+
+      dealer_draw(1)
+
+    elsif @scores[0] == @scores[1] && @scores[3] && @scores[4]
+      #フルハウス狙いで3枚目を交換
+      @discards << @sorts_hands[2]
+
+      dealer_draw(1)
+    
+    elsif @scores[1] == @scores[2] && @scores[3] && @scores[4]
+      #フルハウス狙いで1枚目を交換
+      @discards << @sorts_hands[0]
+
+      dealer_draw(1)
+    
+    end
+  end
+
+  def action_when_a_pair
+    if @scores[0] == @scores[1]
+      #フルハウス狙いで3,4,5枚目を交換
+      @discards.push(@sorts_hands[2], @sorts_hands[3], @sorts_hands[4])
+
+      dealer_draw(3)
+
+    elsif @scores[1] == @scores[2]
+      #フルハウス狙いで1,4,5枚目を交換
+      @discards.push(@sorts_hands[0], @sorts_hands[3], @sorts_hands[4])
+
+      dealer_draw(3)
+
+    elsif @scores[2] == @scores[3]
+      #フルハウス狙いで1,2,5枚目を交換
+      @discards.push(@sorts_hands[0], @sorts_hands[1], @sorts_hands[4])
+
+      dealer_draw(1)
+
+    elsif @scores[3] == @scores[4]
+      #フルハウス狙いで1,2,3枚目を交換
+      @discards.push(@sorts_hands[0], @sorts_hands[1], @sorts_hands[2])
+
+      dealer_draw(1)
+
+    end
+  end
+
+  #ハイカードのときの行動
+  def action_when_high_card
+    #ランダムな枚数交換
+  end
+
+  def dealer_draw(i)
+    discard
+
+    i.times {|hand|
+      hand = deck.draw
+      @hands << hand
+    }
+
+  end
+
+  #dealerの手札公開
+  def display_dealer_hand
+    puts <<~EOS
+
+    -------------Dealer手札-------------
+    EOS
+
+    display_hands
+
+    puts "------------------------------------"
+
   end
 end
 
@@ -385,6 +514,7 @@ class GamesController
     player.judge_ranks
 
     dealer.dealer_exchange(deck)
+    dealer.display_dealer_hand
 
 
   end
